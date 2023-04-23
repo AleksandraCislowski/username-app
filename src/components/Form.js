4346fd6f6afd592292a30eb05d67f3e40a249c2d
aleksandra <aleksandra.swigon91@gmail.com>
2023-04-23 19:21:05 +0200

    testing

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,12 @@ const Form = () => {
   return (
     <div className='form'>
       <label>Username</label>
-      <input type='text'></input>
+      <input type='text' value='Anna'></input>
       <label>Age (Years)</label>
-      <input type='number'></input>
-      <button className='button'>Add User</button>
+      <input type='number' min='0' max='99' step='1' value='32'></input>
+      <button className='button' type='submit'>
+        Add User
+      </button>
     </div>
   );
 };
