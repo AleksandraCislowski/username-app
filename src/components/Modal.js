d1d1d330bb5346a34e856da261af5c087d832d1c
aleksandra <aleksandra.swigon91@gmail.com>
2023-04-25 20:30:36 +0200

    styling modals

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,6 +3,7 @@ import "./Modal.css";
 function Modal({ closeModal, error }) {
   return (
     <div className='modal'>
+      <h2>Invalid input</h2>
       <p>{error}</p>
       <button onClick={closeModal}>Okay</button>
     </div>
